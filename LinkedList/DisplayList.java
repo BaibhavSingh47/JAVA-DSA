278f221d7ec759c810876adc18fd986ffeb80722
Baibhav Singh <baibhavsingh894@gmail.com>
2026-08-30 18:18:26 +0530

    Save changes

--- LinkedList/DisplayList.java
+++ LinkedList/DisplayList.java
@@ -1,22 +1,51 @@
 package LinkedList;
 
+import java.util.Scanner;
+
 public class DisplayList {
-    public static void display(Node head){
-        Node temp=head;
-        while(temp!=null){
-            System.out.println(temp.val+" ");
-            temp=temp.next;
-        }
-        System.out.println();
+    public static void displayRec(Node head) {
+        if (head == null)
+            return;
+        displayRec(head.next);
+        System.out.print(head.val + " ");
     }
+
+    public static void display(Node head) {
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.val + " ");
+            temp = temp.next; // VERY IMPORTANT
+        }
+        // for(Node temp = head;temp != null;temp = temp.next){
+        // System.out.print(temp.val+" ");
+        // }
+        // System.out.println();
+    }
+
     public static void main(String[] args) {
-   Node a=new Node(10);
-   Node b=new Node(20);
-   Node c=new Node(30);
-   Node d=new Node(40);
-   Node e=new Node(50);
-   Node f=new Node(60);
-   a.next=b;b.next=c;c.next=d;d.next=e;e.next=f;
-display(a);
+        Node a = new Node(10); // head node
+        Node b = new Node(200);
+        Node c = new Node(30);
+        Node d = new Node(40);
+        Node e = new Node(50);
+        a.next = b;
+        b.next = c;
+        c.next = d;
+        d.next = e;
+        // display(a);
+        // displayRec(a);
+        System.out.println(get(a, 4));
+        // Node n = null;
+        // n.val = 10;
+        // System.out.println(n.val);
+        // System.out.println(n.next);
+    }
+
+    private static int get(Node head, int idx) {
+        Node temp = head;
+        for (int i = 1; i <= idx; i++) {
+            temp = temp.next;
+        }
+        return temp.val;
     }
 }
